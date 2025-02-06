cfce63cb2c026e0f863c7a132aa340b72cdfb5b4
Robert Becker <robertbecker@Roberts-MacBook-Pro-2.local>
2025-02-05 18:52:35 -0600

    more bug squashing b4 deployment

--- components/ui/alert.tsx
+++ components/ui/alert.tsx
@@ -1,13 +1,23 @@
 import * as React from "react"
+import { cn } from "@/lib/utils"
 
-const Alert = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>((
-  { ...props },
+interface AlertProps extends React.HTMLAttributes<HTMLDivElement> {
+  variant?: "default" | "destructive"
+}
+
+const Alert = React.forwardRef<HTMLDivElement, AlertProps>((
+  { className, variant = "default", ...props },
   ref
 ) => (
   <div
     ref={ref}
     role="alert"
-    className="relative w-full rounded-lg border p-4 [&>svg~*]:pl-7 [&>svg+div]:translate-y-[-3px] [&>svg]:absolute [&>svg]:left-4 [&>svg]:top-4 [&>svg]:text-foreground"
+    className={cn(
+      "relative w-full rounded-lg border p-4 [&>svg~*]:pl-7 [&>svg+div]:translate-y-[-3px] [&>svg]:absolute [&>svg]:left-4 [&>svg]:top-4 [&>svg]:text-foreground",
+      variant === "destructive" && 
+        "border-destructive/50 text-destructive dark:border-destructive [&>svg]:text-destructive",
+      className
+    )}
     {...props}
   />
 ))
@@ -15,12 +25,12 @@ const Alert = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivEleme
 Alert.displayName = "Alert"
 
 const AlertDescription = React.forwardRef<HTMLParagraphElement, React.HTMLAttributes<HTMLParagraphElement>>((
-  { ...props },
+  { className, ...props },
   ref
 ) => (
   <div
     ref={ref}
-    className="text-sm [&_p]:leading-relaxed"
+    className={cn("text-sm [&_p]:leading-relaxed", className)}
     {...props}
   />
 ))
